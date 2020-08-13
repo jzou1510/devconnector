25564af28e993825fc7abf711354123f5b608746
jzou <jzou@vt.edu>
2020-08-13 14:42:58 -0400

    React/concurrently setup and register form

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -244,7 +246,9 @@ router.put(
 			check('school', 'School is required').not().isEmpty(),
 			check('degree', 'Degree is required').not().isEmpty(),
 			check('fieldofstudy', 'Field of study is required').not().isEmpty(),
-			check('from', 'From date is required and needs to be from the past').not().isEmpty()
+			check('from', 'From date is required and needs to be from the past')
+				.not()
+				.isEmpty(),
 		],
 	],
 	async (req, res) => {
@@ -310,4 +314,35 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 		res.status(500).send('Server Error');
 	}
 });
+
+// @route DELETE api/profile/github/:username
+// @desc Get user repos from github
+// @access Public
+router.get('/github/:username', async (req, res) => {
+	try {
+		const options = {
+			uri: `https://api.github.com/users/${
+				req.params.username
+			}/repos?per_page=5&sort=created:asc&client_id=${config.get(
+				'githubClientId'
+			)}&client_secret=${config.get('githubSecret')}`,
+			method: 'GET',
+			headers: { 'user-agent': 'node.js' },
+		};
+
+		request(options, (error, response, body) => {
+			if (error) console.error(error);
+
+			if (response.statusCode !== 200) {
+				return res.status(404).json({ msg: 'No Github profile found' });
+			}
+
+			res.json(JSON.parse(body));
+		});
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
+});
+
 module.exports = router;
